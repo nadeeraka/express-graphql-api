86f0df841cbe4de837a6831547b9e4b77e91bf11
nadeeraka <nadeerakan@gmail.com>
2020-04-22 23:02:37 +0530

    configer

---
+++ src/controllers/index.ts
@@ -0,0 +1,5 @@
+// export const main = (_,res:any) => {
+//     res.json({
+//       message: "hello world",
+//     });
+//   };
--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,23 @@
 import "reflect-metadata";
 import express from "express";
 import { routes } from "./routes";
-import morgan from 'morgan'
+import morgan from "morgan";
+import cors from "cors";
+import helmet from "helmet";
+import bodyParser from "body-parser";
 
 const port: string | number = process.env.port || 8080;
 const init = async () => {
   const app = express();
-  await app.use(morgan('common'))
+  await app.use(morgan("common"));
+  await app.use(bodyParser.urlencoded({ extended: false }));
+  await app.use(
+    cors({
+      origin: "http://localhost:3000",
+    })
+  );
+  await app.use(helmet());
+
   await app.use(routes.router);
 
   await app.listen(port, () => console.log(`server runing on ${port}`));
