1fec0702db85dcf567b4a5677047db3989fc31eb
nadeeraka <nadeerakan@gmail.com>
2020-04-24 22:46:44 +0530

    algo implimenting

--- src/controllers/expenses.ts
+++ src/controllers/expenses.ts
@@ -1,4 +1,3 @@
-
 // ! goal
 // give a  forecast for  given  expenses
 
@@ -7,25 +6,48 @@
 //priotize  expenses
 // find out useless expenses
 
-
 class Expenses {
+  constructor(private _expenses: number[], private _income: number) {}
 
-    constructor(private _expenses: [number] ){
+  set setExpenses(e: number[]) {
+    if (e.length < 0) throw "This Array is not valid";
+    this._expenses = e;
+  }
 
+  set setIncome(i: number) {
+    if (i < 0) throw "This income   not valid";
+    this._income = i;
+  }
+
+  get setExpenses() {
+    return this._expenses;
+  }
+
+  get setIncome() {
+    return this._income;
+  }
+
+  calculateExpenses() {
+    let count: number = 0;
+
+    //   const count = this._expenses.reduce((a:number,b:number)=>a+b)
+    //   return count
+    for (let i = 0; i < this._expenses.length; i++) {
+      count += this._expenses[i];
+      console.log(count);
+    }
+    return count;
+  }
+
+  setBasicGrade() {
+    if (this._income < this.calculateExpenses()) {
+      return -1;
+    } else if (this._income === this.calculateExpenses()) {
+      return 1;
     }
 
-    set expenses (e:[number])
-    {
-        if(e.length < 0)
-        throw "This Array is not valid";
-        this._expenses = e
-    }
+    return 0;
+  }
 
-    get expenses()
-    {
-        return this._expenses
-    }
-    
-
-
+  setAdvanceGrade() {}
 }
