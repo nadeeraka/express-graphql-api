07f7d395de4c7d82eb29a5700dd4005aaff429d6
nadeeraka <nadeerakan@gmail.com>
2020-04-25 18:26:56 +0530

    login bug fix

--- src/graphql/resolvers/Login.ts
+++ src/graphql/resolvers/Login.ts
@@ -1,14 +1,20 @@
 import { Mutation, Resolver, Arg } from "type-graphql";
-import { User } from "src/entity/User";
+import { User } from "../../entity/User";
+import { compare } from "bcryptjs";
 
 @Resolver()
 export class Login {
-  @Mutation(()=>Boolean)
+  @Mutation(() => Boolean)
   async login(@Arg("email") email: string, @Arg("password") password: string) {
-    const user = User.findOne({ where: { email } });
+    const user = await User.findOne({ where: { email } });
 
     if (!user) {
       throw new Error("invalid login");
+    }
+    const isValid = await compare(password, user.password);
+
+    if (!isValid) {
+        throw new Error("bad password !");
     }
   }
 }
