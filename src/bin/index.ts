f442a4315be7b5a3e49907d12a49541a0f9205da
nadeeraka <nadeerakan@gmail.com>
2020-05-01 12:53:27 +0530

    fun add 🥦

--- src/bin/index.ts
+++ src/bin/index.ts
@@ -1,16 +1,15 @@
+import "reflect-metadata";
 import { ApolloServer } from "apollo-server-express";
 import cors from "cors";
-import express from "express";
 import morgan from "morgan";
-import "reflect-metadata";
 import { buildSchema } from "type-graphql";
 import { resolvers } from "../graphql/resolverConfig";
 import { dbConnect } from "../util/db/DButill";
 import { logger } from "../util/logger";
-
+import {main} from '../middlewares'
+import {app} from '../util/middleware/app'
 
 const port: string | number = process.env.port || 8080;
-const app = express();
 export const bootstrap = async () => {
   await app.use(morgan("common"));
   await app.use(
@@ -18,6 +17,8 @@ export const bootstrap = async () => {
       origin: "http://localhost:3000",
     })
   );
+// routes
+ app.use(main)
 
   await dbConnect();
 
---
+++ src/middlewares/index.ts
@@ -0,0 +1,34 @@
+
+
+// import express from "express";
+// const router = express.Router();
+
+// //
+
+// router.use("/", (req, res, next) => {
+//     res.status(200).json({
+//       status: 200,
+//       message: {
+//         greetings: " welcome 🎉",
+//         details: "go to /graphql to query",
+//       },
+//     });
+//   });
+
+// router.get("/login", (req, res) => {
+//     res.json({
+//       message: "login",
+//     });
+//   });
+
+  
+
+// //note found
+// router.use((req, res, next) => {
+//   res.status(404).json({
+//     status: "not found 404!",
+//     emoj: "⛔️",
+//   });
+// });
+
+// module.exports = router;
